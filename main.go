9ea203f0b5f893c7300e9c16fa7245ee061d27d6
oluwaseyi <oluwaseyi_akinnawo@rocketmail.com>
2023-09-08 12:13:16 +0100

    syscall.SIGTERM instead of os.kill

--- main.go
+++ main.go
@@ -1,37 +1,73 @@
 package main
 
 import (
-	// "fmt"
+	"context"
 	"log"
-	"microservices/handlers"
 	"net/http"
-	t "time"
-
-	// "io"
 	"os"
+	"os/signal"
+	t "time"
+	"syscall"
+	// "fmt"
+	// "io"
+
+	"microservices/handlers"
 )
 
 func main() {
+	// Create logger
 	l := log.New(os.Stderr, "product-api", log.LstdFlags)
+
+	// Create handlers for  routes
 	hh := handlers.NewHello(l)
 	gb := handlers.NewGoodbye(l)
 
+	// Create a ServeMux (multiplexer) to handle different route
 	sm := http.NewServeMux()
 	sm.Handle("/", hh)
 	sm.Handle("/goodbye", gb)
 
 
-	// Listen for connections on all ip addresses (0.0.0.0)
-	// port 9090
+	// Create an HTTP server
 	s := &http.Server{
-		Addr:    ":9090",
-        Handler: sm,
-		IdleTimeout:  120 * t.Second,
-		ReadTimeout:  1   *t.Second,
-        WriteTimeout: 1   *t.Second,
+		Addr:          ":9090",
+        Handler:       sm,
+		IdleTimeout:   120 * t.Second,
+		ReadTimeout:   1   *t.Second,
+        WriteTimeout:  1   *t.Second,
 	}
 
-	s.ListenAndServe()
+
+	// Start the server in a goroutine
+	go func() {
+        log.Println("Listening on port 9090")
+		err := s.ListenAndServe()
+		if err!= nil {
+            l.Fatal(err)
+        }
+    }()
+
+	// Set up a signal channel to gracefully handle termination signals
+	sigChan := make(chan os.Signal, 1)
+	// added a buffer of 1
+
+
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+	// Use syscall instead of os.Kill
+	
+
+	sig := <-sigChan
+	l.Println("Recieved Terminate Signal, shutting down gracefully", sig)
+
+	
+
+	// Create a context with a timeout
+	tc, cancel := context.WithTimeout(context.Background(), 30*t.Second)
+	defer cancel()	
+		
+	if err:= s.Shutdown(tc); err != nil {
+		l.Fatalf("Error during server shutdown: %v", err)
+	}
 
 	
 	// log.Println("Starting Server")
